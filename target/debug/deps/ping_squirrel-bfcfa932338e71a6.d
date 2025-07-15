7d208f8f4f5aea352fbe3da4641b1a355d80ddad
Toby <toby@tfaucher.com>
2025-07-14 19:36:52 -0600

    Added some dependencies ➕

---
+++ target/debug/deps/ping_squirrel-bfcfa932338e71a6.d
@@ -0,0 +1,9 @@
+/home/toby/Projects/PingSquirrel/target/debug/deps/ping_squirrel-bfcfa932338e71a6.d: src/main.rs Cargo.toml
+
+/home/toby/Projects/PingSquirrel/target/debug/deps/libping_squirrel-bfcfa932338e71a6.rmeta: src/main.rs Cargo.toml
+
+src/main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
+# env-dep:CLIPPY_CONF_DIR

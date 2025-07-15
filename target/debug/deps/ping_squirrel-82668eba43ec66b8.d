1f2089a504918301518e2f743b1a8b4bfda01d58
Toby <toby@tfaucher.com>
2025-07-14 19:41:35 -0600

    Stop tracking target directory

--- target/debug/deps/ping_squirrel-82668eba43ec66b8.d
+++
@@ -1,9 +0,0 @@
-/home/toby/Projects/PingSquirrel/target/debug/deps/ping_squirrel-82668eba43ec66b8.d: src/main.rs Cargo.toml
-
-/home/toby/Projects/PingSquirrel/target/debug/deps/libping_squirrel-82668eba43ec66b8.rmeta: src/main.rs Cargo.toml
-
-src/main.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=
-# env-dep:CLIPPY_CONF_DIR
